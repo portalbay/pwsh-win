4c9fd4e5cf2c8aef610939700604a809e382aae6
Andrew Davis <andrew.davis@vinebrookhomes.com>
2021-11-09 13:59:47 -0500

    Updated myip

--- public_profile.ps1
+++ public_profile.ps1
@@ -1,2 +1,49 @@
 #
 Set-Alias -Name gim -Value Get-InstalledModule
+
+function b{
+		vim $PROFILE
+}
+
+function sb {
+		. $profile
+}
+
+function install_vbh_modules
+{
+		Install-Module -Name AzureAD
+			Install-Module -Name ExchangeOnlineManagement
+				Install-Module -Name Microsoft.Graph
+}
+
+function me {
+		(iwr ifconfig.me).content
+}
+
+function ifconfig {
+		ipconfig 
+}
+
+
+function fx {
+		        start firefox '-P'
+}
+
+
+function dt {
+		cd  ($ENV:onedrive + "\Desktop")
+}
+
+function wp {
+	cd  ($ENV:onedrive + "\Documents\WindowsPowerShell")
+
+}
+
+function azpass ($azUserID)
+{
+	Import-Module AzureAD
+	Write-Host "Connecting to Azure AD"
+	Connect-AzureAD
+	$Secure = Read-Host "Enter New Password" -AsSecureString
+	Set-AzureADUserPassword -ObjectId $azUserID -Password $Secure
+}
