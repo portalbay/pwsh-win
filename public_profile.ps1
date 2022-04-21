6168259e3c6f573dac630662b61b4705792e2ce7
scv_ready <74455344+portalbay@users.noreply.github.com>
2022-04-21 08:05:11 -0400

    Update public_profile.ps1

--- public_profile.ps1
+++ public_profile.ps1
@@ -1,4 +1,4 @@
-
+#Main entry point, loads public_profile.ps1 for various functions and Aliases 
 if (Get-Variable 'powershell_work_directory' -ErrorAction 'Ignore') {
 	  #Write-Host "loading.." + $powershell_work_directory
 } else {
@@ -6,7 +6,7 @@ if (Get-Variable 'powershell_work_directory' -ErrorAction 'Ignore') {
 	Exit
 }
 
-
+#Alias Defined Here
 Set-Alias -Name gim -Value Get-InstalledModule
 
 function b{
@@ -25,23 +25,20 @@ function pb {
         vim.exe ($powershell_work_directory + "public_profile.ps1")
 }
 
-
 function install_vbh_modules
 {
 	Install-Module -Name AzureAD -Scope CurrentUser
 	Install-Module -Name ExchangeOnlineManagement -Scope CurrentUser
 	Install-Module -Name Microsoft.Graph -Scope CurrentUser
-	 Install-Module MSOnline -Scope CurrentUser
+        Install-Module MSOnline -Scope CurrentUser
 }
 
 function search_module_path($name)
 {
-
 	(Get-Module -ListAvailable $name*).path
 }
 
 function me {
-		#(iwr ifconfig.me).content
 	myip
 }
 
@@ -50,20 +47,18 @@ function myip{
 	$prvip = Test-Connection -ComputerName (hostname) -Count 1	
 	$prvip = $prvip.IPV4Address.IPAddressToString
 	$h = hostname
-	Write-Host "IPv4 Public: $pubip" -ForegroundColor green -BackgroundColor blue
-	Write-Host "IPv4 Local: $prvip" -ForegroundColor green -BackgroundColor blue
-	Write-Host ("Hostname: " + $h ) -ForegroundColor green -BackgroundColor blue
+	Write-Host "IPv4 Public: $pubip" -ForegroundColor Black -BackgroundColor Green
+	Write-Host "IPv4 Local: $prvip" -ForegroundColor Black -BackgroundColor Green
+	Write-Host ("Hostname: " + $h ) -ForegroundColor Black -BackgroundColor Green
 }
 
 function ifconfig {
 	ipconfig 
 }
 
-
 function fx {
 	start firefox '-P'
 }
-
 
 function dt {
 	cd  ($ENV:onedrive + "\Desktop")
@@ -78,16 +73,7 @@ function wp {
 
 }
 
-function azpass ($azUserID)
-{
-	Import-Module AzureAD
-	Write-Host "Connecting to Azure AD"
-	Connect-AzureAD
-	$Secure = Read-Host "Enter New Password" -AsSecureString
-	Set-AzureADUserPassword -ObjectId $azUserID -Password $Secure
-}
-
-
+#Git commands here
 function gitgo ($note) {
 	git commit -m $note
 	gitpush
@@ -107,12 +93,25 @@ function gillpull_allow_unrelated_histories{
 	git pull --allow-unrelated-histories
 }
 
+#Pull latest public profile script from github
 function update_env()
 {
 	Write-Host "Welcome back $env:UserName don't work too hard.." -ForegroundColor green
 	Write-Host "Updating .win powershell scripts." -ForegroundColor red
 	cd  $powershell_work_directory
 	gitpull
+}
+
+#Azure Functions here
+
+
+function azpass ($azUserID)
+{
+	Import-Module AzureAD
+	Write-Host "Connecting to Azure AD"
+	Connect-AzureAD
+	$Secure = Read-Host "Enter New Password" -AsSecureString
+	Set-AzureADUserPassword -ObjectId $azUserID -Password $Secure
 }
 
 function azcon{
@@ -127,14 +126,7 @@ function az_check_liscense{
 	Write-Host ($azu_license_counts)
 }
 
-
-
-#Functions to run 
-#update_env
-
-
-
-
+#Wirelss Functions Here
 function get_ssid_creds($ssid) {
 	netsh wlan show profile name=$ssid key=clear
 
